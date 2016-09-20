21190bef2dc6b73fad373235ce6e29d7a278efce
zhibin.liu <yimogod@gmail.com>
2016-09-20 16:39:24 +0800

    init clip plane project

---
+++ trunk/learn/T13ClipPlane/DemoApp.cpp
@@ -0,0 +1,41 @@
+#include <util/ObjParser.h>
+#include "DemoApp.h"
+
+DemoApp::DemoApp(){
+	_scroll.scroll = 0;
+}
+
+DemoApp::~DemoApp(){}
+
+bool DemoApp::LoadContent(){
+	ObjParser reader;
+	reader.Read(GetFullPath("assets/cube.obj").c_str(), &_scene);
+	_scene.renderType = Scene::RENDER_TYPE_FRAME;
+	_scene.camera = &_camera;
+
+	/*准备顶点缓冲数据*/
+	_currMesh = _scene.GetMesh(0);
+	InitVisual(_currMesh, L"shader/clip_plane.fx", "assets/t_01.dds");
+	return true;
+}
+
+void DemoApp::PreAddOtherConstBuffer(DXVisual &visual){
+	visual.PreSetVSConstBufferSize(sizeof(ScrollBuffer));
+}
+
+void DemoApp::UnloadContent(){}
+
+void DemoApp::Update(){
+	Window::Update();
+
+	_scroll.scroll += 0.0001f;
+	_dxEngine.UpdateVSSubResource(GetVisual(), 1, &_scroll);
+}
+
+void DemoApp::Render(){
+	if(!_dxEngine.GetReady())return;
+
+	_dxEngine.ClearBuffers();
+	_dxEngine.DrawVisual(GetVisual());
+	_dxEngine.Present();
+}
---
+++ trunk/learn/T13ClipPlane/DemoApp.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <windows.h>
+#include <Main.h>
+#include <sys/Window.h>
+
+struct ScrollBuffer{
+	float scroll;
+	float pad0, pad1, pad2;
+};
+
+class DemoApp : public Window{
+public:
+	DemoApp();
+	virtual ~DemoApp();
+
+	virtual bool LoadContent();
+	virtual void PreAddOtherConstBuffer(DXVisual &visual);
+	virtual void UnloadContent();
+
+	virtual void Update();
+	virtual void Render();
+
+private:
+	ScrollBuffer _scroll;
+};
+
+int WINAPI wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR lpCmdLine,
+	_In_ int nCmdShow){
+	MainInfo info;
+	info.app = new DemoApp();
+	info.icon = nullptr;
+	info.title = L"Clip Plane";
+	info.width = 1200;
+	info.height = 960;
+
+	return MainBody(hInstance, hPrevInstance, lpCmdLine, nCmdShow, info);
+}
