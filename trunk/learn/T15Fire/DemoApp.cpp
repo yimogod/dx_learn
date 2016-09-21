917de1be1c9a7c1e9d889e607c40d004aa1285b5
zhibin.liu <yimogod@gmail.com>
2016-09-21 16:14:33 +0800

    new fire project

---
+++ trunk/learn/T15Fire/DemoApp.cpp
@@ -0,0 +1,49 @@
+#include <util/ObjParser.h>
+#include "DemoApp.h"
+
+DemoApp::DemoApp(){
+	_scroll.scroll = 0;
+	_fade.scroll = 0;
+	_tag = 0.0001f;
+}
+
+DemoApp::~DemoApp(){}
+
+bool DemoApp::LoadContent(){
+	ObjParser reader;
+	reader.Read(GetFullPath("assets/cube.obj").c_str(), &_scene);
+	_scene.renderType = Scene::RENDER_TYPE_FRAME;
+	_scene.camera = &_camera;
+
+	/*准备顶点缓冲数据*/
+	_currMesh = _scene.GetMesh(0);
+	InitVisual(_currMesh, L"shader/fire.fx", "assets/t_01.dds");
+	_dxEngine.EnableAlphaBlend();
+	return true;
+}
+
+void DemoApp::PreAddOtherConstBuffer(DXVisual &visual){
+	visual.PreSetVSConstBufferSize(sizeof(ScrollBuffer));
+	visual.PreSetVSConstBufferSize(sizeof(ScrollBuffer));
+}
+
+void DemoApp::UnloadContent(){}
+
+void DemoApp::Update(){
+	Window::Update();
+
+	_scroll.scroll += 0.0001f;
+	_dxEngine.UpdateVSSubResource(GetVisual(), 1, &_scroll);
+	if(_fade.scroll > 1.0f || _fade.scroll < 0.0f)_tag *= -1.0f;
+
+	_fade.scroll += _tag;
+	_dxEngine.UpdateVSSubResource(GetVisual(), 2, &_fade);
+}
+
+void DemoApp::Render(){
+	if(!_dxEngine.GetReady())return;
+
+	_dxEngine.ClearBuffers();
+	_dxEngine.DrawVisual(GetVisual());
+	_dxEngine.Present();
+}
---
+++ trunk/learn/T15Fire/DemoApp.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <windows.h>
+#include <Main.h>
+#include <sys/Window.h>
+
+struct ScrollBuffer{
+	float scroll;
+	float pad0, pad1, pad2;
+};
+
+class DemoApp : public Window{
+public:
+	DemoApp();
+	virtual ~DemoApp();
+
+	virtual bool LoadContent();
+	virtual void PreAddOtherConstBuffer(DXVisual &visual);
+	virtual void UnloadContent();
+
+	virtual void Update();
+	virtual void Render();
+
+private:
+	ScrollBuffer _scroll;
+	ScrollBuffer _fade;
+	float _tag = 0.1f;
+};
+
+int WINAPI wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR lpCmdLine,
+	_In_ int nCmdShow){
+	MainInfo info;
+	info.app = new DemoApp();
+	info.icon = nullptr;
+	info.title = L"Scroll UV";
+	info.width = 1200;
+	info.height = 960;
+
+	return MainBody(hInstance, hPrevInstance, lpCmdLine, nCmdShow, info);
+}
